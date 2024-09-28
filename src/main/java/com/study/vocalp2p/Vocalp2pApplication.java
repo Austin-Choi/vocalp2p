87d91de154d6b4f53ef25ff17e76ed28d8de33e3
Austin-Choi <austinsupra@gmail.com>
2024-09-28 10:37:06 +0900

    project initialize

---
+++ src/main/java/com/study/vocalp2p/Vocalp2pApplication.java
@@ -0,0 +1,13 @@
+package com.study.vocalp2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vocalp2pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vocalp2pApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/vocalp2p/Vocalp2pApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.vocalp2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vocalp2pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
