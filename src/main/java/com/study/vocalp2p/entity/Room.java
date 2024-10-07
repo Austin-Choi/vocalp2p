1f90dbb2ddc72d55a771faaa4ca27305fb752c0d
Austin-Choi <austinsupra@gmail.com>
2024-10-07 20:59:57 +0900

    patch : fixed wrong collation to ko

--- src/main/java/com/study/vocalp2p/entity/Room.java
+++ src/main/java/com/study/vocalp2p/entity/Room.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
-@Document(collation = "rooms")
+@Document(collation = "ko")
 @Getter
 @Builder(toBuilder = true)
 @NoArgsConstructor
