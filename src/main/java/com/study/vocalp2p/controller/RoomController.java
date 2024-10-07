f824766ab09d7c1ff65450cc8c93dbc72f80c03a
Austin-Choi <austinsupra@gmail.com>
2024-10-07 21:02:18 +0900

    feat : added joinRoom(establishing webRTC connection) feature

--- src/main/java/com/study/vocalp2p/controller/RoomController.java
+++ src/main/java/com/study/vocalp2p/controller/RoomController.java
@@ -1,8 +1,10 @@
 package com.study.vocalp2p.controller;
 
 import com.study.vocalp2p.config.BaseResponse;
+import com.study.vocalp2p.dto.JoinRoomDTO;
 import com.study.vocalp2p.entity.Room;
 import com.study.vocalp2p.service.RoomService;
+import com.study.vocalp2p.util.RoomStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -17,17 +19,42 @@ public class RoomController {
         this.roomService = roomService;
     }
 
-    // Caller의 이메일을 받아 방을 생성하고 URL을 반환
+    /**
+     * 방을 생성함
+     * @param callerEmail
+     * @return UUID가 포함된 고유한 방 URL 반환
+     */
     @PostMapping
     public BaseResponse<String> createRoom(@RequestParam("callerEmail") String callerEmail){
         Room newRoom = roomService.createRoom(callerEmail);
-        return new BaseResponse<>("http://localhost:3000/room/" + newRoom.getRoomId());
+        // navigate에서 상대결로로 설정해야 리다이렉션됨
+        return new BaseResponse<>(newRoom.getRoomId());
     }
 
+    /**
+     * 방 정보 불러오기
+     * @param roomId
+     * @return 해당 id로 조회되는 room 객체 데이터
+     */
     @GetMapping("/{roomId}")
     public BaseResponse<Room> getRoom(@PathVariable String roomId){
         return new BaseResponse<>(roomService.getRoomByRoomId(roomId));
     }
 
     // update랑 delete는 webRTC 흐름에 따라 내부적으로 일어나기 때문에 controller에는 없음.
+
+    /**
+     * RoomId, email과 함께 방 입장
+     * @param joinRoomDTO
+     * @return 현재 입장한 방 status
+     */
+    @PostMapping("/room/join")
+    public BaseResponse<String> joinRoomByLink(@RequestBody JoinRoomDTO joinRoomDTO){
+        Room updatedRoom = roomService.joinRoom(joinRoomDTO.getRoomId(), joinRoomDTO.getEmail());
+        return new BaseResponse<>(
+                roomService.updateRoomStatus(
+                        updatedRoom.getRoomId(),
+                        RoomStatus.INCALL.getLabel())
+                        .getStatus());
+    }
 }
